ba82163dead68cd33ca080855a577d0275508c13
Anshul Arora <anshularora@anshul.arora>
2022-04-24 14:30:42 +0530

    draw file added

---
+++ test/src/test/dtdl.java
@@ -0,0 +1,20 @@
+package test;
+
+public class dtdl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		
+		int arr[] = {1,2,3,41,5};
+		
+		for(int k:arr) {
+			
+			sum = sum+k;
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
